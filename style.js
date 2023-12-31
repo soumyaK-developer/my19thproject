452ce408b75d096e34bf05eb282efbee2ddaac4f
soumya <iamsoumyakantasahoo@gmail.com>
2023-12-31 16:41:39 +0530

    modal pop-up is ready

---
+++ style.js
@@ -0,0 +1,26 @@
+console.log(`hello, i am soumya!`)
+
+document.getElementById("myBtn").onclick = function () {
+    let span = document.createElement("span")
+    let txt = document.createTextNode("\u00D7")
+    span.className = "close"
+    span.appendChild(txt)
+    let modalHeader = document.getElementById("modalHeader")
+    modalHeader.appendChild(span)
+
+    var div = document.getElementById("myModal")
+    div.style.display = "block"
+
+
+    let close = document.querySelector(".close")
+    close.onclick = function () {
+        var div = document.getElementById("myModal")
+        div.style.display = "none"
+    }
+    // document.body.onclick =  function() {
+    //     var div = document.getElementById("myModal")
+    //     div.style.display = "none"
+    // }
+}
+
+
